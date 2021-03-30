62684ace4534d653f2762351e332eb31f7f255c8
Gabriela Schlemper <gabschlemper@gmail.com>
2021-03-30 15:29:06 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const numbers = [1,2,3,4,5,6,7,8,9];
+  const operations = ["+", "-", "*", "/"]
+
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
+
+  const [operation, setOperation] = useState("");
+
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    console.log(result);
+  }, [result]);
+
+  function clickNumbers(val) {
+    if (operation == "") {
+      setFirstNumber(firstNumber + val)
+    } else {
+      setSecondNumber(secondNumber + val)
+    }
+  }
+
+  function clickOperation(val) {
+    setOperation(val);
+  }
+
+  function perforOperation() {
+    switch (operation) {
+      case "+":
+        setResult(Number(firstNumber) + Number(secondNumber));
+        break;
+      case "-":
+        setResult(Number(firstNumber) - Number(secondNumber));
+        break;
+      case "*":
+        setResult(Number(firstNumber) * Number(secondNumber));
+        break;
+      case "/":
+        setResult(Number(firstNumber) / Number(secondNumber));
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">{result}</div>
+        <div className="buttons">
+          <div className="leftSide">
+            <div className="clean">AC</div>
+            <div id="seeResult" onClick={perforOperation}>
+              =
+            </div>
+            <div className="numbers">
+              {numbers.map((val, key)=> {
+                return (
+                  <div 
+                    id="individualNumber" 
+                    onClick={()=> {
+                      clickNumbers(val)
+                      }}
+                  > 
+                      {" "}
+                      {val}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          
+          <div className="rightSide">
+              {operations.map((val, key)=> {
+                return ( 
+                  <div 
+                    id="operations" 
+                    onClick={()=> {
+                      clickOperation(val)
+                    }}
+                  >
+                    {val}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
